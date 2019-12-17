068cb95f887b7c73b2ddfe5a4d4e3bdf2dbf3173
Monica Amitin <monzielvsu@gmail.com>
2019-12-16 18:14:40 -0800

    created functions for bamazon customer

--- bamazon.sql
+++ bamazon.sql
@@ -5,12 +5,35 @@ USE bamazonDB;
 
 CREATE TABLE products (
     id INT AUTO_INCREMENT NOT NULL,
-    product_name VARCHAR(50) NOT NULL,
+    product_name VARCHAR(100) NOT NULL,
     department_name VARCHAR(50) NOT NULL,
-    price DECIMAL(20, 2) NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
     stock_quantity INT NOT NULL,
     PRIMARY KEY (id)
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("iPad", "Electronics", 399.99, 4), ("Bose Speakers", "Electronics", 200.00, 7), ("iPhone", "Phones", 1199.00, 25)
+VALUES ("iPad", "Electronics", 399.99, 4),
+("Bose Speakers", "Electronics", 200.49, 7),
+("iPhone 11", "Phones", 1199.99, 25),
+("Alexa TV Remote", "Electronics", 149.99, 50),
+("Apple MacBook Air", "Computers", 699.95, 17),
+("Samsung Galaxy Note 10", "Phones", 899.99, 14),
+("Toshiba 32in LED HDTV", "TV", 129.99, 9),
+("Samsung 65in 4K TV", "TV", 479.99, 10),
+("Microsoft Surface Laptop 3", "Computers", 799.00, 11),
+("PlayStation 4", "Video Games", 299.99, 4),
+("Nintendo Switch", "Video Games", 299.99, 5);
+
+CREATE TABLE departments(
+    department_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    department_name VARCHAR(50) NOT NULL,
+    over_head_costs DECIMAL(10,2) NOT NULL
+);
+
+INSERT INTO departments(department_name, over_head_costs)
+VALUES ("Electronics", 500),
+("Phones", 1000),
+("Computers", 1500),
+("TV", 700),
+("Video Games", 2000);
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 //imports & modules
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require("cli-table");
 
 //connection to mysql database
 var connection = mysql.createConnection({
@@ -15,35 +16,63 @@ var connection = mysql.createConnection({
 
 //establish connection
 connection.connect(function (err) {
-    if (err) throw err;
-    console.log("Connection to database works.")
+    if (err) throw err
+    console.log("\n----Welcome to Bamazon!----\n");
+    loadProducts();
 });
 
 //load products table
-connection.query("SELECT * FROM products", function (err, response) {
-    console.table(response);
-})
+function loadProducts() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        var table = new Table({
+            head: ["ID#", "Product", "Price", "Quantity"]
+        });
+        for (i = 0; i < res.length; i++) {
+            table.push(
+                [res[i].item_id, res[i].product_name, res[i].price, res[i].stock_quantity]
+            )
+        }
+        console.log(table.toString() + "\n");
+        orderPrompt();
+    });
+};
 
-
-inquirer.prompt([
-    {
-        message: "Please enter ID of product you want.",
+function orderPrompt() {
+    inquirer.prompt([{
+        name: "productID",
         type: "input",
-        name: "productID"
+        message: "What is the ID of product you want?",
+        validate: function (value) {
+            if (isNaN(value) === false) {
+                return true;
+            } else {
+                console.log("Please enter a number!");
+                return false;
+            }
+        }
     },
     {
-        message: "How many would you like to buy?",
+        name: "quantity",
         type: "input",
-        name: "quantity"
-    }
-]).then(function (answer) {
-    //console.log(answer);
-    var id = +answer.productID
-    //console.log(id);
-    connection.query("SELECT * products WHERE products.id = ?", [id], function (err, response) {
-        console.table(response);
+        message: "How many of this item would you like to buy?",
+        validate: function (value) {
+            if (isNaN(value) === false) {
+                return true;
+            } else {
+                console.log("Please enter a number!");
+                return false;
+            }
+        }
+    }]).then(function (answer) {
+        //console.log(answer);
+        var id = +answer.productID
+        //console.log(id);
+        connection.query("SELECT * products WHERE id = "+ productID, function (err, response) {
+            console.table(response);
+        })
     })
-})
+}
 
 
 
